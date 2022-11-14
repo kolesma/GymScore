aa5a506b75032a758a092b22f6572e1f9ce6b176
Maria <kolesovamariie@gmail.com>
2022-11-14 18:30:07 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <RouterView></RouterView>
+</template>
+<style>
+h1{
+  color: red;
+}
+
+</style>
+<script>
+export default{
+  name:"App",
+  data: function (){
+    return {count:0}
+  },
+  methods: {
+    add: function (){
+      this.count++
+    }
+  }
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <a href="/login">Login
+    </a>
+  </main>
+</template>
