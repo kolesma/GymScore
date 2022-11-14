a6514303be5aa20f64c6b8a96073b46479beec64
Maria <kolesovamariie@gmail.com>
2022-11-14 18:59:11 +0100

    Added Auth

--- src/main.js
+++ src/main.js
@@ -13,6 +13,23 @@ import '@quasar/extras/material-icons/material-icons.css'
 // Import Quasar css
 import 'quasar/src/css/index.sass'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBO2HxxJF2OsES2eNo31pX2gH953RXdfAQ",
+    authDomain: "vuefs-prod-f327a.firebaseapp.com",
+    projectId: "vuefs-prod-f327a",
+    storageBucket: "vuefs-prod-f327a.appspot.com",
+    messagingSenderId: "710035190403",
+    appId: "1:710035190403:web:2c6ad14f2945aa5d5721b6",
+    measurementId: "G-MWMZKEQSBG"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+
 const app = createApp(App)
 
 app.use(createPinia())
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,31 @@
 <template>
-
+<input name="email" v-model="email"/>
+  <input name="password" v-model="password" type="password"/>
+  <button @click="login">Sent</button>
 </template>
 
 <style>
 
 </style>
+<script>
+
+import {
+  signInWithEmailAndPassword,
+    getAuth
+} from "firebase/auth"
+import {
+ getApp
+} from "firebase/app"
+
+export default {
+  name: "LoginView",
+  data:()=>({email:"",password:""}),
+  methods:{
+    async login(){
+      console.log(this.email)
+      let user= await  signInWithEmailAndPassword(getAuth(),this.email, this.password)
+      console.log(user)
+    }
+  }
+}
+</script>
